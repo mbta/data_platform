9ff21fdf008d5de5403b57d4efc97decdeb7355e
Grejdi Gjura <ggjura@mbta.com>
2022-02-15 18:12:32 -0500

    fix format

--- ex_cubic_ods_ingestion/lib/ex_cubic_ods_ingestion/application.ex
+++ ex_cubic_ods_ingestion/lib/ex_cubic_ods_ingestion/application.ex
@@ -13,13 +13,16 @@ defmodule ExCubicOdsIngestion.Application do
        run_migrations_at_startup?:
          Application.get_env(:ex_cubic_ods_ingestion, :run_migrations_at_startup?)}
     ]
-    app_children = if Application.get_env(:ex_cubic_ods_ingestion, :start_app?) do
-      [
-        {ExCubicOdsIngestion.ProcessIncoming, []}
-      ]
-    else
-      []
-    end
+
+    app_children =
+      if Application.get_env(:ex_cubic_ods_ingestion, :start_app?) do
+        [
+          {ExCubicOdsIngestion.ProcessIncoming, []}
+        ]
+      else
+        []
+      end
+
     children = db_children ++ app_children
 
     # See https://hexdocs.pm/elixir/Supervisor.html
