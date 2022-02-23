360994554ddd56ae10c510242008c710c21e3fc3
Grejdi Gjura <ggjura@mbta.com>
2022-02-23 10:40:19 -0500

    provide default at the env level.

--- ex_cubic_ods_ingestion/config/runtime.exs
+++ ex_cubic_ods_ingestion/config/runtime.exs
@@ -1,11 +1,11 @@
 import Config
 
 config :ex_cubic_ods_ingestion,
-  s3_prefix_operations: System.get_env("S3_PREFIX_OPERATIONS"),
-  s3_prefix_incoming: System.get_env("S3_PREFIX_INCOMING"),
-  s3_prefix_archive: System.get_env("S3_PREFIX_ARCHIVE"),
-  s3_prefix_error: System.get_env("S3_PREFIX_ERROR"),
-  s3_prefix_springboard: System.get_env("S3_PREFIX_SPRINGBOARD"),
+  s3_prefix_operations: System.get_env("S3_PREFIX_OPERATIONS", ""),
+  s3_prefix_incoming: System.get_env("S3_PREFIX_INCOMING", ""),
+  s3_prefix_archive: System.get_env("S3_PREFIX_ARCHIVE", ""),
+  s3_prefix_error: System.get_env("S3_PREFIX_ERROR", ""),
+  s3_prefix_springboard: System.get_env("S3_PREFIX_SPRINGBOARD", ""),
   s3_bucket_operations: System.get_env("S3_BUCKET_OPERATIONS"),
   s3_bucket_incoming: System.get_env("S3_BUCKET_INCOMING"),
   s3_bucket_archive: System.get_env("S3_BUCKET_ARCHIVE"),
