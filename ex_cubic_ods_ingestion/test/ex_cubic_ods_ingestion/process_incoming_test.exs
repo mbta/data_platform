5b3a0694ca5fd1461995f9141fff3c6ddb7f2387
Paul Swartz <paul@paulswartz.net>
2022-03-10 08:54:10 -0500

    test: add test case for `ProcessIncoming.run/1`

--- ex_cubic_ods_ingestion/test/ex_cubic_ods_ingestion/process_incoming_test.exs
+++ ex_cubic_ods_ingestion/test/ex_cubic_ods_ingestion/process_incoming_test.exs
@@ -14,4 +14,39 @@ defmodule ExCubicOdsIngestion.ProcessIncomingTest do
       assert ProcessIncoming.status(server) == :running
     end
   end
+
+  describe "run/1" do
+    setup do
+      {:ok, state, _timeout} = ProcessIncoming.init(lib_ex_aws: MockExAws)
+
+      {:ok, state: state}
+    end
+
+    test "does nothing without configured tables", %{state: state} do
+      :ok = ProcessIncoming.run(state)
+
+      assert Repo.all(CubicOdsLoad) == []
+    end
+
+    test "with a configured table, scans the prefixes for files", %{state: state} do
+      table = Repo.insert!(MockExAws.Data.table())
+      table_id = table.id
+
+      :ok = ProcessIncoming.run(state)
+
+      assert [load1, load2] = Repo.all(CubicOdsLoad)
+
+      assert %CubicOdsLoad{
+               s3_key: "cubic_ods_qlik/SAMPLE/LOAD1.csv",
+               status: "ready",
+               table_id: ^table_id
+             } = load1
+
+      assert %CubicOdsLoad{
+               s3_key: "cubic_ods_qlik/SAMPLE/LOAD2.csv",
+               status: "ready",
+               table_id: ^table_id
+             } = load2
+    end
+  end
 end
