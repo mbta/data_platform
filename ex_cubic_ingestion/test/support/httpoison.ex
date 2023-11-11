8f40579c5559a479ac63320c3fbfc0ae06a47d0c
Grejdi Gjura <ggjura@mbta.com>
2023-11-11 13:57:34 -0500

    feat: hide any url details if dmap errors on fetch

--- ex_cubic_ingestion/test/support/httpoison.ex
+++ ex_cubic_ingestion/test/support/httpoison.ex
@@ -3,44 +3,57 @@ defmodule MockHTTPoison do
   Allow for controlling what is returned for a HTTPoison request.
   """
 
-  @spec get!(String.t()) :: HTTPoison.Response.t()
-  def get!(url) do
+  @spec get(String.t()) :: {:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}
+  def get(url) do
     dmap_base_url = Application.fetch_env!(:ex_cubic_ingestion, :dmap_base_url)
 
     cond do
       String.starts_with?(url, "#{dmap_base_url}/datasetpublicusersapi/sample") ->
-        %HTTPoison.Response{
-          status_code: 200,
-          body: """
-          {
-            "success": true,
-            "results": [
-              {
-                "id": "sample",
-                "dataset_id": "sample_20220517",
-                "url": "https://mbtaqadmapdatalake.blob.core.windows.net/sample/sample_2022-05-17.csv.gz",
-                "start_date": "2022-05-17",
-                "end_date": "2022-05-17",
-                "last_updated": "2022-05-18T13:39:43.546303"
-              },
-              {
-                "id": "sample",
-                "dataset_id": "sample_20220518",
-                "url": "https://mbtaqadmapdatalake.blob.core.windows.net/sample/sample_2022-05-18.csv.gz",
-                "start_date": "2022-05-18",
-                "end_date": "2022-05-18",
-                "last_updated": "2022-05-19T12:12:44.737440"
-              }
-            ]
-          }
-          """
-        }
+        {:ok,
+         %HTTPoison.Response{
+           status_code: 200,
+           body: """
+           {
+             "success": true,
+             "results": [
+               {
+                 "id": "sample",
+                 "dataset_id": "sample_20220517",
+                 "url": "https://mbtaqadmapdatalake.blob.core.windows.net/sample/sample_2022-05-17.csv.gz",
+                 "start_date": "2022-05-17",
+                 "end_date": "2022-05-17",
+                 "last_updated": "2022-05-18T13:39:43.546303"
+               },
+               {
+                 "id": "sample",
+                 "dataset_id": "sample_20220518",
+                 "url": "https://mbtaqadmapdatalake.blob.core.windows.net/sample/sample_2022-05-18.csv.gz",
+                 "start_date": "2022-05-18",
+                 "end_date": "2022-05-18",
+                 "last_updated": "2022-05-19T12:12:44.737440"
+               }
+             ]
+           }
+           """
+         }}
 
-      String.starts_with?(url, "https://mbtaqadmapdatalake.blob.core.windows.net/sample") ->
-        %HTTPoison.Response{status_code: 200, body: "sample_body"}
+      String.starts_with?(url, "#{dmap_base_url}/datasetpublicusersapi/error") ->
+        # bad request
+        {:ok, %HTTPoison.Response{status_code: 400, body: "{}"}}
 
       true ->
-        %HTTPoison.Response{status_code: 404, body: ""}
+        {:error, %HTTPoison.Error{}}
+    end
+  end
+
+  @spec get!(String.t()) :: HTTPoison.Response.t()
+  def get!(url) do
+    dmap_base_url = Application.fetch_env!(:ex_cubic_ingestion, :dmap_base_url)
+
+    if String.starts_with?(url, "https://mbtaqadmapdatalake.blob.core.windows.net/sample") do
+      %HTTPoison.Response{status_code: 200, body: "sample_body"}
+    else
+      %HTTPoison.Response{status_code: 404, body: ""}
     end
   end
 end
