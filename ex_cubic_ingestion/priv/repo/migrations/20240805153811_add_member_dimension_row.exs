76fba4a135704133c1de7fd090c78d4f6be48494
Grejdi Gjura <ggjura@mbta.com>
2024-08-05 12:56:57 -0400

    feat: add member dimension table for processing

---
+++ ex_cubic_ingestion/priv/repo/migrations/20240805153811_add_member_dimension_row.exs
@@ -0,0 +1,29 @@
+defmodule ExCubicIngestion.Repo.Migrations.AddMemberDimensionRow do
+  use Ecto.Migration
+
+  alias ExCubicIngestion.Repo
+  alias ExCubicIngestion.Schema.CubicTable
+  alias ExCubicIngestion.Schema.CubicOdsTableSnapshot
+
+  @ods_table_name "cubic_ods_qlik__edw_member_dimension"
+  @ods_table_s3_prefix "cubic/ods_qlik/EDW.MEMBER_DIMENSION/"
+
+  def up do
+    ods_table_rec = Repo.insert!(%CubicTable{
+      name: @ods_table_name,
+      s3_prefix: @ods_table_s3_prefix,
+      is_active: true,
+      is_raw: true
+    })
+    Repo.insert!(%CubicOdsTableSnapshot{
+      table_id: ods_table_rec.id,
+      snapshot_s3_key: "#{@ods_table_s3_prefix}LOAD00000001.csv.gz"
+    })
+  end
+
+  def down do
+    ods_table_rec = CubicTable.get_by!(name: @ods_table_name)
+    Repo.delete!(ods_table_rec)
+    Repo.delete!(CubicOdsTableSnapshot.get_by!(table_id: ods_table_rec.id))
+  end
+end
