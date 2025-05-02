4dea133f87f08fd5c31854abd734b040dfc1accc
Grejdi Gjura <ggjura@mbta.com>
2025-05-02 10:02:04 -0400

    chore: add revenue loss assessment table

---
+++ ex_cubic_ingestion/priv/repo/migrations/20250502135830_add_rev_loss_table.exs
@@ -0,0 +1,29 @@
+defmodule ExCubicIngestion.Repo.Migrations.AddRevLossTable do
+  use Ecto.Migration
+
+  alias ExCubicIngestion.Repo
+  alias ExCubicIngestion.Schema.CubicTable
+  alias ExCubicIngestion.Schema.CubicOdsTableSnapshot
+
+  @ods_table_name "cubic_ods_qlik__edw_revenue_loss_assessment"
+  @ods_table_s3_prefix "cubic/ods_qlik/EDW.REVENUE_LOSS_ASSESSMENT/"
+
+  def up do
+    ods_table_rec = Repo.insert!(%CubicTable{
+      name: @ods_table_name,
+      s3_prefix: @ods_table_s3_prefix,
+      is_active: true,
+      is_raw: true
+    })
+    Repo.insert!(%CubicOdsTableSnapshot{
+      table_id: ods_table_rec.id,
+      snapshot_s3_key: "#{@ods_table_s3_prefix}LOAD00000001.csv.gz"
+    })
+  end
+
+  def down do
+    ods_table_rec = CubicTable.get_by!(name: @ods_table_name)
+    Repo.delete!(ods_table_rec)
+    Repo.delete!(CubicOdsTableSnapshot.get_by!(table_id: ods_table_rec.id))
+  end
+end
