e850f4ddee3b0fb57480cded93fde36c5bad37d5
Ted Alexander <talexander3@mbta.com>
2025-10-31 13:39:51 -0400

    Adding EDW.TRANSIT_ACCOUNT_BALANCE

---
+++ ex_cubic_ingestion/priv/repo/migrations/20251030204555_adding-tables-20251030.exs
@@ -0,0 +1,43 @@
+defmodule ExCubicIngestion.Repo.Migrations.AddMoreTablesForIngestion do
+  use Ecto.Migration
+
+  alias ExCubicIngestion.Repo
+  alias ExCubicIngestion.Schema.CubicTable
+  alias ExCubicIngestion.Schema.CubicOdsTableSnapshot
+
+  @ods_tables [
+    %{
+      name: "cubic_ods_qlik__edw_transit_account_balance",
+      s3_prefix: "cubic/ods_qlik/EDW.TRANSIT_ACCOUNT_BALANCE/"
+    },
+  ]
+
+  def up do
+    Repo.transaction(fn ->
+      Enum.each(@ods_tables, fn ods_table ->
+        ods_table_rec =
+          Repo.insert!(%CubicTable{
+            name: ods_table[:name],
+            s3_prefix: ods_table[:s3_prefix],
+            is_active: true,
+            is_raw: true
+          })
+
+        Repo.insert!(%CubicOdsTableSnapshot{
+          table_id: ods_table_rec.id,
+          snapshot_s3_key: "#{ods_table[:s3_prefix]}LOAD00000001.csv.gz"
+        })
+      end)
+    end)
+  end
+
+  def down do
+    Repo.transaction(fn ->
+      Enum.each(@ods_tables, fn ods_table ->
+        ods_table_rec = CubicTable.get_by!(name: ods_table[:name])
+        Repo.delete!(ods_table_rec)
+        Repo.delete!(CubicOdsTableSnapshot.get_by!(table_id: ods_table_rec.id))
+      end)
+    end)
+  end
+end
